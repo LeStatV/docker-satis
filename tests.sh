d053679351aa129e833a6fb62459b81fa40b26ad
LeStatV <lestat@funs.net.ua>
2018-06-22 12:25:23 +0300

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -10,8 +10,10 @@ function check_errors() {
 }
 
 function build() {
-  docker build -t "${IMAGE_NAME}:${VERSION}" .
+  docker build -t "${IMAGE_NAME}:${VERSION}" ./
   check_errors $?
+  docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+  docker push $(IMAGE_NAME):$(VERSION)
 }
 
 function run() {
